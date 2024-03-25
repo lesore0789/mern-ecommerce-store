1d18180b739dcd2386cc4246448d56429bb3439c
Leslie Orellana <lesore0789@gmail.com>
2024-03-25 16:16:10 -0700

    Finished Navbar and Sidebar on Admin page

---
+++ admin/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import './Admin.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+
+const Admin = () => {
+  return (
+    <div className='admin'>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Admin
