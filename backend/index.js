7bcb47daf94ab24ed02a234903bd454dbc80d9b6
Leslie Orellana <lesore0789@gmail.com>
2024-03-24 19:59:04 -0700

    multer config for upload enpoint for images

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,30 @@ app.get("/", (req, res) => {
   res.send("express app running");
 });
 
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating Upload Endpoint for Images
+app.use("/images", express.static("upload/images"));
+
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server running on Port " + port);
