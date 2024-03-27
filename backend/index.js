97308e8434566ba85297b17ee053decb939f2451
Leslie Orellana <lesore0789@gmail.com>
2024-03-27 15:30:49 -0700

    cart tied to user functionality and endpoints

--- backend/index.js
+++ backend/index.js
@@ -221,6 +221,53 @@ app.get("/popularinwomen", async (req, res) => {
   res.send(popularInWomen);
 });
 
+// middleware to fetch user
+const fetchUser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using valid token" });
+  } else {
+    try {
+      const data = jwt.verify(token, "secret_ecom");
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res.status(401).send({ errors: "Please authenticate using valid token" });
+    }
+  }
+};
+
+// Endpoint for adding products in cart
+app.post("/addtocart", fetchUser, async (req, res) => {
+  let userData = await Users.findOne({ _id: req.user.id });
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Added");
+});
+
+// Endpoint to remove product from cart
+app.post("/removefromcart", fetchUser, async (req, res) => {
+  let userData = await Users.findOne({ _id: req.user.id });
+  if (userData.cartData[req.body.itemId] > 0) {
+    userData.cartData[req.body.itemId] -= 1;
+  }
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Removed");
+});
+
+// Endpoint to get cartdata
+app.post("/getcart", fetchUser, async (req, res) => {
+  console.log("getcart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  res.json(userData.cartData);
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server running on Port " + port);
